2a2804a50afdb45f4f425d4f625bbd3d27100b02
luislr220 <largoramirezluisarmando@gmail.com>
2025-12-25 13:43:38 -0600

    Ajustando los dto

--- DTOs/UsuarioActualizarDto.cs
+++ DTOs/UsuarioActualizarDto.cs
@@ -8,11 +8,11 @@ public class UsuarioActualizarDto
     [MaxLength(50, ErrorMessage = "El nombre no debe contener más de 50 caracteres.")]
     [RegularExpression(@"^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s]+$",
     ErrorMessage = @"El campo contiene caracteres no permitidos. Evite usar símbolos como < > / \ & % $")]
-    public string? NombreUsuario { get; set; } = string.Empty;
+    public string? NombreUsuario { get; set; }
 
     [EmailAddress(ErrorMessage = "El formato de correo no es el esperado. e.j: example@example.com")]
-    public string? Correo { get; set; } = string.Empty;
+    public string? Correo { get; set; }
 
     [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres")]
-    public string? Contrasena { get; set; } = string.Empty;
+    public string? Contrasena { get; set; }
 }
--- DTOs/UsuarioLeerDto.cs
+++ DTOs/UsuarioLeerDto.cs
@@ -4,9 +4,9 @@ public class UsuarioLeerDto
 {
 
     
-    public string? NombreUsuario { get; set; } = string.Empty;
+    public string? NombreUsuario { get; set; }
 
-    public string? Correo { get; set; } = string.Empty;
+    public string? Correo { get; set; }
     public List<ProyectoReadDto> Proyectos {get; set;} = new();
 
 }
